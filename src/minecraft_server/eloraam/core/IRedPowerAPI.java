dc9c12917c2393e57acdd214b8d78ea651bb119b
Kleadron <kleadron04@gmail.com>
2020-06-06 20:18:29 -0700

    fixed missing redpower api in server

---
+++ src/minecraft_server/eloraam/core/IRedPowerAPI.java
@@ -0,0 +1,13 @@
+//Dummy File
+
+package eloraam.core;
+
+public interface IRedPowerAPI {
+	public int getConnectableMask();
+
+    public int getPowerClass(int var1);
+
+    public int getCornerPowerMode();
+
+    public int getPoweringMask(int var1);
+}
