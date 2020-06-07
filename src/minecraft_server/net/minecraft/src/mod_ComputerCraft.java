1838cadda53269cbd0aaab7a45d9760bf9081b32
Kleadron <kleadron04@gmail.com>
2020-06-06 21:59:46 -0700

    Computercraft server working!

--- src/minecraft_server/net/minecraft/src/mod_ComputerCraft.java
+++ src/minecraft_server/net/minecraft/src/mod_ComputerCraft.java
@@ -48,7 +48,6 @@ public class mod_ComputerCraft extends BaseModMp {
     public static BlockComputer computer;
     public static BlockDiskDrive diskDrive;
     public static ItemDisk disk;
-    private static int m_tickCount;
 	
     public static int[] loadTerrainTextures(String folder, int count) {
         int[] result = new int[count];
@@ -66,56 +65,39 @@ public class mod_ComputerCraft extends BaseModMp {
 	
 	public mod_ComputerCraft() {
 		instance = this;
-        System.out.println("ComputerCraft: computerBlockID " + computerBlockID);
-        System.out.println("ComputerCraft: diskDriveBlockID " + diskDriveBlockID);
-        System.out.println("ComputerCraft: diskItemID " + diskItemID);
-        System.out.println("ComputerCraft: To change IDs, modify config/mod_ComputerCraft.cfg");
-        computer = new BlockComputer(computerBlockID);
-        computer.setHardness(1.0f).setBlockName("computer"); //.setRequiresSelfNotify(); does not exist in this version, might be why redstone doesn't work?
-        ModLoader.RegisterBlock((Block)computer);
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Block)computer, 1), (Object[])new Object[]{"XXX", "XYX", "XZX", Character.valueOf('X'), Block.stone, Character.valueOf('Y'), Item.redstone, Character.valueOf('Z'), Block.glass});
-        ModLoader.RegisterTileEntity((Class)RedPowerInterop.getComputerClass(), (String)"computer");
-        diskDrive = new BlockDiskDrive(diskDriveBlockID);
-        diskDrive.setHardness(1.0f).setBlockName("diskdrive"); //.setRequiresSelfNotify();
-        ModLoader.RegisterBlock((Block)diskDrive);
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Block)diskDrive, 1), (Object[])new Object[]{"XXX", "XYX", "XYX", Character.valueOf('X'), Block.stone, Character.valueOf('Y'), Item.redstone});
-        ModLoader.RegisterTileEntity(TileEntityDiskDrive.class, (String)"diskdrive");
-        disk = new ItemDisk(diskItemID);
-        disk.setItemName("disk");
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Item)disk, 1), (Object[])new Object[]{"X", "Y", Character.valueOf('X'), Item.redstone, Character.valueOf('Y'), Item.paper});
-        
-        //testing recipes
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Item)disk, 1), (Object[])new Object[]{"XX", "X ", 'X', Block.dirt});
-        ModLoader.AddRecipe((ItemStack)new ItemStack(Item.recordCat, 1), (Object[])new Object[]{"X", "X", 'X', Block.dirt});
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Block)diskDrive, 1), (Object[])new Object[]{"XX", 'X', Block.dirt});
-        ModLoader.AddRecipe((ItemStack)new ItemStack((Block)computer, 1), (Object[])new Object[]{"X", 'X', Block.dirt});
-
-        m_tickCount = 0;
-        ModLoader.SetInGameHook((BaseMod)this, (boolean)true, (boolean)true);
-        
-        //load doesn't execute normally so i'm putting it here, it's also pretty important
-        load();
+	}
+	
+	public void ModsLoaded() {
+		super.ModsLoaded();
+	    System.out.println("ComputerCraft: computerBlockID " + computerBlockID);
+	    System.out.println("ComputerCraft: diskDriveBlockID " + diskDriveBlockID);
+	    System.out.println("ComputerCraft: diskItemID " + diskItemID);
+	    System.out.println("ComputerCraft: To change IDs, modify config/mod_ComputerCraft.cfg");
+	    computer = new BlockComputer(computerBlockID);
+	    computer.setHardness(1.0f).setBlockName("computer"); //.setRequiresSelfNotify(); does not exist in this version, might be why redstone doesn't work?
+	    ModLoader.RegisterBlock((Block)computer);
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Block)computer, 1), (Object[])new Object[]{"XXX", "XYX", "XZX", Character.valueOf('X'), Block.stone, Character.valueOf('Y'), Item.redstone, Character.valueOf('Z'), Block.glass});
+	    ModLoader.RegisterTileEntity((Class)RedPowerInterop.getComputerClass(), (String)"computer");
+	    diskDrive = new BlockDiskDrive(diskDriveBlockID);
+	    diskDrive.setHardness(1.0f).setBlockName("diskdrive"); //.setRequiresSelfNotify();
+	    ModLoader.RegisterBlock((Block)diskDrive);
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Block)diskDrive, 1), (Object[])new Object[]{"XXX", "XYX", "XYX", Character.valueOf('X'), Block.stone, Character.valueOf('Y'), Item.redstone});
+	    ModLoader.RegisterTileEntity(TileEntityDiskDrive.class, (String)"diskdrive");
+	    disk = new ItemDisk(diskItemID);
+	    disk.setItemName("disk");
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Item)disk, 1), (Object[])new Object[]{"X", "Y", Character.valueOf('X'), Item.redstone, Character.valueOf('Y'), Item.paper});
+	     
+	    //testing recipes
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Item)disk, 1), (Object[])new Object[]{"XX", "X ", 'X', Block.dirt});
+	    ModLoader.AddRecipe((ItemStack)new ItemStack(Item.recordCat, 1), (Object[])new Object[]{"X", "X", 'X', Block.dirt});
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Block)diskDrive, 1), (Object[])new Object[]{"XX", 'X', Block.dirt});
+	    ModLoader.AddRecipe((ItemStack)new ItemStack((Block)computer, 1), (Object[])new Object[]{"X", 'X', Block.dirt});
+	    ItemDisk.loadLabels(null);
 	}
 
 	public String getPriorities() {
         return "";
     }
-
-    public void load() {
-        MinecraftServer mc = ModLoader.getMinecraftServerInstance();
-        mod_ComputerCraft.computer.blockTextures = mod_ComputerCraft.loadTerrainTextures("computer", 4);
-        mod_ComputerCraft.diskDrive.blockTextures = mod_ComputerCraft.loadTerrainTextures("diskdrive", 5);
-        mod_ComputerCraft.disk.iconIndex = ModLoader.addOverride((String)"/gui/items.png", (String)"/items/floppy.png");
-    }
-	
-    public static void openDiskDriveGUI(EntityPlayer entityplayer, TileEntityDiskDrive drive) {
-        ContainerDiskDrive container = new ContainerDiskDrive(entityplayer.inventory, drive);
-        ModLoader.OpenGUI((EntityPlayer)entityplayer, (int)diskDriveGUIID, entityplayer.inventory, (Container)container);
-    }
-    
-	public static boolean getCursorBlink() {
-		return (m_tickCount / 6 % 2 == 0);
-	}
 
 	public static void notifyBlockChange(World world, int x, int y, int z, int b) {
 	    world.notifyBlockChange(x, y, z, b);
@@ -137,8 +119,15 @@ public class mod_ComputerCraft extends BaseModMp {
         return true;
     }
 
+    public static void openDiskDriveGUI(EntityPlayer entityplayer, TileEntityDiskDrive drive) {
+        ContainerDiskDrive container = new ContainerDiskDrive((IInventory)entityplayer.inventory, drive);
+        ModLoader.OpenGUI((EntityPlayer)entityplayer, (int)diskDriveGUIID, (IInventory)entityplayer.inventory, (Container)container);
+    }
+
     public static void sendToPlayer(EntityPlayer player, Packet230ModLoader packet) {
-        ModLoaderMp.SendPacketTo((BaseModMp)instance, (EntityPlayerMP)player, (Packet230ModLoader)packet);
+        if (player instanceof EntityPlayerMP) {
+            ModLoaderMp.SendPacketTo((BaseModMp)instance, (EntityPlayerMP)((EntityPlayerMP)player), (Packet230ModLoader)packet);
+        }
     }
 
     public static void sendToAllPlayers(Packet230ModLoader packet) {
@@ -149,13 +138,13 @@ public class mod_ComputerCraft extends BaseModMp {
         instance.HandlePacket(packet, null);
     }
 
-    public void HandlePacket(Packet230ModLoader packet, EntityPlayer player) {
-        World world = ModLoaderMp.GetPlayerWorld(player);
+    public void HandlePacket(Packet230ModLoader packet, EntityPlayerMP player) {
+        World world = ModLoaderMp.GetPlayerWorld((EntityPlayer)player);
         if (world != null) {
             if (packet.packetType == 11) {
                 for (int n = 0; n < packet.dataInt.length; ++n) {
                     int id = packet.dataInt[n];
-                    ItemDisk.sendDiskLabelToPlayer((EntityPlayerMP)player, id);
+                    ItemDisk.sendDiskLabelToPlayer((EntityPlayer)player, id);
                 }
             } else {
                 int i = packet.dataInt[0];
@@ -164,7 +153,7 @@ public class mod_ComputerCraft extends BaseModMp {
                 TileEntity entity = world.getBlockTileEntity(i, j, k);
                 if (entity != null && entity instanceof IComputerCraftEntity) {
                     IComputerCraftEntity iComputerCraftEntity = (IComputerCraftEntity)entity;
-                    iComputerCraftEntity.handlePacket(packet, (EntityPlayerMP)player);
+                    iComputerCraftEntity.handlePacket(packet, (EntityPlayer)player);
                 }
             }
         }
